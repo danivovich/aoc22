3ccc6f6b0363237cbf7baa8b29d05772aaf4c6a3
Dan Ivovich <divovich@gmail.com>
2022-12-01 09:56:45 -0500

    Even more refactoring

--- day01/part1.exs
+++ day01/part1.exs
@@ -1,45 +1,23 @@
 defmodule Day01 do
-  defp parse(s) do
-    case Integer.parse(s) do
-      {i, _} ->
-        i
-
-      _ ->
-        nil
-    end
-  end
-
-  defp data do
+  defp elfs do
     {:ok, contents} = File.read("input.txt")
 
     contents
-    |> String.split("\n")
+    |> String.split("\n\n")
     |> Enum.map(fn s ->
-      parse(s)
+      String.split(s, "\n", trim: true)
+      |> Enum.map(fn s ->
+        {i, _} = Integer.parse(s)
+        i
+      end)
+      |> Enum.sum()
     end)
   end
 
-  defp next_acc({sum, max}, nil) when sum > max do
-    {0, sum}
-  end
-
-  defp next_acc({_sum, max}, nil) do
-    {0, max}
-  end
-
-  defp next_acc({sum, max}, n) do
-    {sum + n, max}
-  end
-
   def main do
-    data = data()
-
-    {_sum, max} =
-      Enum.reduce(data, {0, 0}, fn n, acc ->
-        next_acc(acc, n)
-      end)
-
-    IO.puts(max)
+    elfs()
+    |> Enum.max()
+    |> IO.puts()
   end
 end
 
--- day01/part2.exs
+++ day01/part2.exs
@@ -1,41 +1,21 @@
 defmodule Day01Part2 do
-  defp parse(s) do
-    case Integer.parse(s) do
-      {i, _} ->
-        i
-
-      _ ->
-        nil
-    end
-  end
-
-  defp data do
+  defp elfs do
     {:ok, contents} = File.read("input.txt")
 
     contents
-    |> String.split("\n")
+    |> String.split("\n\n")
     |> Enum.map(fn s ->
-      parse(s)
+      String.split(s, "\n", trim: true)
+      |> Enum.map(fn s ->
+        {i, _} = Integer.parse(s)
+        i
+      end)
+      |> Enum.sum()
     end)
   end
 
-  defp next_acc({sum, list}, nil) do
-    {0, [sum | list]}
-  end
-
-  defp next_acc({sum, list}, n) do
-    {sum + n, list}
-  end
-
   def main do
-    data = data()
-
-    {_sum, list} =
-      Enum.reduce(data, {0, []}, fn n, acc ->
-        next_acc(acc, n)
-      end)
-
-    list
+    elfs()
     |> Enum.sort()
     |> Enum.reverse()
     |> Enum.take(3)
