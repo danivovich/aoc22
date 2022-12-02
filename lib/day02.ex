e2c744299586c2106d36b2387b3153e698150cee
Dan Ivovich <divovich@gmail.com>
2022-12-02 08:10:17 -0500

    Day 2 part 1

---
+++ lib/day02.ex
@@ -0,0 +1,71 @@
+defmodule Day02 do
+  @moduledoc """
+  AOC2022 Day 2
+  """
+
+  @doc """
+  Parse input
+
+  ## Examples
+
+      iex> Day02.parse("A Y\\nB X\\nC Z")
+      [["A", "Y"], ["B", "X"], ["C", "Z"]]
+
+  """
+  def parse(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn s ->
+      String.split(s, " ", trim: true)
+    end)
+  end
+
+  def input do
+    {:ok, contents} = File.read("inputs/day02_input.txt")
+    parse(contents)
+  end
+
+  @doc """
+  Readme example
+
+  ## Examples
+
+      iex> Day02.example()
+      15
+
+  """
+  def example() do
+    Day02.parse("A Y\nB X\nC Z")
+    |> Day02.score()
+  end
+
+  def score(input) do
+    Enum.map(input, fn hands ->
+      match(hands)
+    end)
+    |> Enum.sum()
+  end
+
+  # You Win
+  def match(["A", "Y"]), do: 8
+  def match(["B", "Z"]), do: 9
+  def match(["C", "X"]), do: 7
+
+  # Opponent Wins
+  def match(["A", "Z"]), do: 3
+  def match(["B", "X"]), do: 1
+  def match(["C", "Y"]), do: 2
+
+  # Draws
+  def match(["A", "X"]), do: 4
+  def match(["B", "Y"]), do: 5
+  def match(["C", "Z"]), do: 6
+
+  def part1 do
+    input()
+    |> score()
+  end
+
+  def part2 do
+  end
+end
---
+++ test/day02_test.exs
@@ -0,0 +1,12 @@
+defmodule Day02Test do
+  use ExUnit.Case
+  doctest Day02
+
+  test "checks part 1" do
+    assert Day02.part1() == 14375
+  end
+
+  test "checks part 2" do
+    assert Day02.part2() == nil
+  end
+end
