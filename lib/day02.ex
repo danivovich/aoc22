bd0156f3b9a4bb668dd6e05c35a7c37a692938b7
Dan Ivovich <divovich@gmail.com>
2022-12-02 16:01:59 -0500

    Clean up refactor

--- lib/day02.ex
+++ lib/day02.ex
@@ -25,85 +25,97 @@ defmodule Day02 do
     parse(contents)
   end
 
-  @doc """
-  Readme example part1
+  defmodule Part1 do
+    @moduledoc """
+    AOC2022 Day 2 Part 1
+    """
 
-  ## Examples
+    @doc """
+    Readme example part1
 
-      iex> Day02.examplepart1()
-      15
+    ## Examples
 
-  """
-  def examplepart1() do
-    Day02.parse("A Y\nB X\nC Z")
-    |> Day02.scorepart1()
+        iex> Day02.Part1.example()
+        15
+
+    """
+    def example() do
+      Day02.parse("A Y\nB X\nC Z")
+      |> score()
+    end
+
+    def score(input) do
+      Enum.map(input, fn hands ->
+        match(hands)
+      end)
+      |> Enum.sum()
+    end
+
+    # You Win
+    def match(["A", "Y"]), do: 8
+    def match(["B", "Z"]), do: 9
+    def match(["C", "X"]), do: 7
+
+    # Opponent Wins
+    def match(["A", "Z"]), do: 3
+    def match(["B", "X"]), do: 1
+    def match(["C", "Y"]), do: 2
+
+    # Draws
+    def match(["A", "X"]), do: 4
+    def match(["B", "Y"]), do: 5
+    def match(["C", "Z"]), do: 6
   end
 
-  def scorepart1(input) do
-    Enum.map(input, fn hands ->
-      matchp1(hands)
-    end)
-    |> Enum.sum()
+  defmodule Part2 do
+    @moduledoc """
+    AOC2022 Day 2 Part 2
+    """
+
+    @doc """
+    Readme example part2
+
+    ## Examples
+
+        iex> Day02.Part2.example()
+        12
+
+    """
+    def example() do
+      Day02.parse("A Y\nB X\nC Z")
+      |> score()
+    end
+
+    def score(input) do
+      Enum.map(input, fn hands ->
+        match(hands)
+      end)
+      |> Enum.sum()
+    end
+
+    # You Win
+    def match(["A", "Z"]), do: 8
+    def match(["B", "Z"]), do: 9
+    def match(["C", "Z"]), do: 7
+
+    # Opponent Wins
+    def match(["A", "X"]), do: 3
+    def match(["B", "X"]), do: 1
+    def match(["C", "X"]), do: 2
+
+    # Draws
+    def match(["A", "Y"]), do: 4
+    def match(["B", "Y"]), do: 5
+    def match(["C", "Y"]), do: 6
   end
-
-  # You Win
-  def matchp1(["A", "Y"]), do: 8
-  def matchp1(["B", "Z"]), do: 9
-  def matchp1(["C", "X"]), do: 7
-
-  # Opponent Wins
-  def matchp1(["A", "Z"]), do: 3
-  def matchp1(["B", "X"]), do: 1
-  def matchp1(["C", "Y"]), do: 2
-
-  # Draws
-  def matchp1(["A", "X"]), do: 4
-  def matchp1(["B", "Y"]), do: 5
-  def matchp1(["C", "Z"]), do: 6
 
   def part1 do
     input()
-    |> scorepart1()
+    |> Part1.score()
   end
-
-  @doc """
-  Readme example part2
-
-  ## Examples
-
-      iex> Day02.examplepart2()
-      12
-
-  """
-  def examplepart2() do
-    Day02.parse("A Y\nB X\nC Z")
-    |> Day02.scorepart2()
-  end
-
-  def scorepart2(input) do
-    Enum.map(input, fn hands ->
-      matchp2(hands)
-    end)
-    |> Enum.sum()
-  end
-
-  # You Win
-  def matchp2(["A", "Z"]), do: 8
-  def matchp2(["B", "Z"]), do: 9
-  def matchp2(["C", "Z"]), do: 7
-
-  # Opponent Wins
-  def matchp2(["A", "X"]), do: 3
-  def matchp2(["B", "X"]), do: 1
-  def matchp2(["C", "X"]), do: 2
-
-  # Draws
-  def matchp2(["A", "Y"]), do: 4
-  def matchp2(["B", "Y"]), do: 5
-  def matchp2(["C", "Y"]), do: 6
 
   def part2 do
     input()
-    |> scorepart2()
+    |> Part2.score()
   end
 end
