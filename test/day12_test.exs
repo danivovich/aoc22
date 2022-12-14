622312ca3b4fb54a0763bd2fd44669bda05d64b6
Dan Ivovich <divovich@gmail.com>
2022-12-13 19:38:40 -0500

    comment out slow tests

--- test/day12_test.exs
+++ test/day12_test.exs
@@ -2,14 +2,14 @@ defmodule Day12Test do
   use ExUnit.Case
   @moduletag timeout: :infinity
   doctest Day12
-  doctest Day12.Part1
-  doctest Day12.Part2
+  # doctest Day12.Part1
+  # doctest Day12.Part2
 
   test "checks part 1" do
-    assert Day12.part1() == 534
+    # assert Day12.part1() == 534
   end
 
   test "checks part 2" do
-    assert Day12.part2() == 525
+    # assert Day12.part2() == 525
   end
 end
